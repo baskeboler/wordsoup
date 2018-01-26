45fe62e2806c644281492a5db9f7560d70fdff27
Victor Gil <baskeboler@gmail.com>
2018-01-26 04:55:41 -0300

    doc package file

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package wordsoup provides crossword puzzle generating functions as well
+// as loading a dictionary of words from files that can be used for random
+// generation of crossword puzzles.
+package wordsoup
